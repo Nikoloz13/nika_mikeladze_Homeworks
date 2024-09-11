482eeddac9e4a1b29517f3e9dd26b95e8c806b95
Nikusha04 <nmikeladze83@gmail.com>
2024-09-11 21:44:31 +0400

    Homework1

---
+++ Homework1/Homework1/task_1.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name: ")
+surname = input("Enter Your Surname: ")
+age = int(input("Enter Your Age: "))
+
+print(f"{name} {surname}, you are {age} years old!!! ")
---
+++ Homework1/Homework1/task_2.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name: ")
+dateOfBirth = int(input("Enter your date of birth: "))
+age = 2024 - dateOfBirth
+
+print(f"{name}, you are {age} years old!!! ")
