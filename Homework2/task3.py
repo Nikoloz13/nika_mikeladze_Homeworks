bd736dba43523a6d2e8eaedd4711d7259ad172c0
Nikusha04 <nmikeladze83@gmail.com>
2024-09-16 20:58:09 +0400

    Changing importing.

--- Homework2/task3.py
+++ Homework2/task3.py
@@ -1,4 +1,4 @@
-import math # რა იქნება ამ შემთხვევაში სპეციფიკური იმპორტი ?
+from math import log, log2 
 
 num = int(input("Enter a number from 1 to 10: "))
 half = int(num / 2)
@@ -6,10 +6,10 @@ half = int(num / 2)
 if(num > 0 and num <= 10):
     if(num == 1):
         print('1' * 2)
-    elif(math.log2(num).is_integer() and num / 2 != 1):
-        print('2' * int(math.log2(num)))
-    elif(math.log(num,3).is_integer() and num / 3 != 1):
-        print('3' * int(math.log(num,3)))
+    elif(log2(num).is_integer() and num / 2 != 1):
+        print('2' * int(log2(num)))
+    elif(log(num,3).is_integer() and num / 3 != 1):
+        print('3' * int(log(num,3)))
     elif(num % 2 == 0):
         print('2' + str(half))
     else:
