0f1e5fbd412082022897b88087eaa7de1ef1468b
Nikusha04 <nmikeladze83@gmail.com>
2024-09-18 17:39:14 +0400

    Revert "Merge pull request #1 from Nikoloz13/Homework1"
    This reverts commit 9781b77fa205e88e4511114da85f91392b74612e, reversing
    changes made to e8f379ab4c4b6a1f8f94400d23c0697671426643.

--- Homework1/Homework1/task_1.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter Your Name: ")
-surname = input("Enter Your Surname: ")
-age = int(input("Enter Your Age: "))
-
-print(f"{name} {surname}, you are {age} years old!!! ")
--- Homework1/Homework1/task_2.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter your Name: ")
-date_of_birth = int(input("Enter your date of birth: "))
-age = 2024 - date_of_birth
-
-print(f"{name}, you are {age} years old!!! ")
