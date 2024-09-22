2429279c2ee542373c62bc5761c5da28388f9e7c
Nikusha04 <nmikeladze83@gmail.com>
2024-09-22 21:30:11 +0400

    Update task2
    პირობას რო გადავხედე გამომრჩენია მაქსიმუმის პოვნა, ეს არის დასრულებული task2

--- Homework4/task2.py
+++ Homework4/task2.py
@@ -6,5 +6,13 @@ if num <= 0 or num >= 30:
     print("Invalid Input")
     exit(1)
 
+max_num = 0
+
 for i in range (0, num):
-    print(randint(1, 1000))
+    random_num = randint(1, 1000)
+    print(random_num)
+
+    if random_num > max_num:
+        max_num = random_num
+
+print("The maximum number is:", max_num)        
